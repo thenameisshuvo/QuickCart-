51590cb9104398b77297ff37321490f60311b970
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2024-09-22 00:28:05 +0600

    created files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './*.js'],  // Include paths to HTML and JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
